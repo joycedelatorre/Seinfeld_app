279a0394153897f63b2389331cba087140a85a08
joyce <joycedelatorre87@gmail.com>
2018-03-28 19:45:13 -0500

    add installs and server.js, integrate server with DB

--- server.js
+++ server.js
@@ -14,8 +14,32 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err){
 	if (err){
-		console.error("error connecting " + err.stock);
+		console.error("error connecting " + err.stack);
 		return;
 	}
-	console.log("connected as id " + connection threadId);
+	console.log("connected as id " + connection.threadId);
 });
+
+app.get("/", function (req, res){
+	//if they main route is hit, then we initiate a SQL query to grab all records.
+	//all of the resulting records are stored in the variable "result"
+	connection.query("SELECT * FROM actors", function (err, results){
+		//We then build out html elements for the page.
+		var html ="<h1> Seinfeld Actors </h1>"
+		//Here we begin an unordered list
+		html += "<ul>"
+		for(var i=0; i< results.length; i++){
+			html += "<li><p> ID: " + results[i].id + "</p>";
+			html += "<p> actor: " + results[i].name + "</p>";
+			html += "<p> actor: " + results[i].coolness_points + "</p>";
+			html += "<p> actor: " + results[i].attitude + "</p></li>";
+		}
+		//we close our unordered list
+		html += "</ul>";
+		//finally we send the user the HTML file we dynamically created.
+		res.send(html);
+	});
+});
+
+//Initiate the listener
+	app.listen(port);
