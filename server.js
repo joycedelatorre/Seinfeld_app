4cd294f46ce417fcbb08f5a126bf20204bfc19d1
joyce <joycedelatorre87@gmail.com>
2018-03-28 23:28:13 -0500

    add optional query for routes

--- server.js
+++ server.js
@@ -31,9 +31,53 @@ app.get("/", function (req, res){
 		for(var i=0; i< results.length; i++){
 			html += "<li><p> ID: " + results[i].id + "</p>";
 			html += "<p> actor: " + results[i].name + "</p>";
-			html += "<p> actor: " + results[i].coolness_points + "</p>";
-			html += "<p> actor: " + results[i].attitude + "</p></li>";
+			html += "<p> coolness_points: " + results[i].coolness_points + "</p>";
+			html += "<p> attitude: " + results[i].attitude + "</p></li>";
 		}
+		//we close our unordered list
+		html += "</ul>";
+		//finally we send the user the HTML file we dynamically created.
+		res.send(html);
+	});
+});
+
+app.get("/coolness_points", function (req, res){
+	//if they main route is hit, then we initiate a SQL query to grab all records.
+	//all of the resulting records are stored in the variable "result"
+	connection.query("SELECT * FROM actors ORDER BY coolness_points DESC", function (err, results){
+		//We then build out html elements for the page.
+		var html ="<h1> Seinfeld Actors </h1>"
+		//Here we begin an unordered list
+		html += "<ul>"
+		for(var i=0; i< results.length; i++){
+			html += "<li><p> ID: " + results[i].id + "</p>";
+			html += "<p> actor: " + results[i].name + "</p>";
+			html += "<p> coolness_points: " + results[i].coolness_points + "</p>";
+			html += "<p> attitude: " + results[i].attitude + "</p></li>";
+		}
+		//we close our unordered list
+		html += "</ul>";
+		//finally we send the user the HTML file we dynamically created.
+		res.send(html);
+	});
+});
+
+app.get("/:attitude", function (req, res){
+	//if they main route is hit, then we initiate a SQL query to grab all records.
+	//all of the resulting records are stored in the variable "result"
+	var att = req.params.attitude;
+	connection.query("SELECT * FROM actors WHERE attitude ='"+att+"'", function (err, results){
+		//We then build out html elements for the page.
+		var html ="<h1> Seinfeld Actors </h1>"
+		//Here we begin an unordered list
+		html += "<ul>"
+		html += "<li><p> ID: " + results[0].name + "</p></li>";
+		// for(var i=0; i< results.length; i++){
+			// html += "<li><p> ID: " + results.id + "</p>";
+			// html += "<p> actor: " + results.name + "</p>";
+			// html += "<p> actor: " + results.coolness_points + "</p>";
+			// html += "<p> actor: " + results.attitude + "</p></li>";
+		// }
 		//we close our unordered list
 		html += "</ul>";
 		//finally we send the user the HTML file we dynamically created.
